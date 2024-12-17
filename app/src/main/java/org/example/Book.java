1037b67d8ac59223175338f6bc25c1f5790a4258
Elipio <elipiokanyiri88@gmail.com>
2024-12-17 15:30:43 +0000

    update to work

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,23 +1,5 @@
-public class Book {
-    private String title;
-    private String author;
-    private boolean borrowed;
+package org.example 
 
-    public Book(String title, String author) {
-        this.title = title;
-        this.author = author;
-        this.borrowed = false; // Initially, the book is not borrowed.
-    }
+public record Book(String title, String author, int yearPublished) {
 
-    public void borrow() {
-        borrowed = true;
-    }
-
-    public void returnBook() {
-        borrowed = false;
-    }
-
-    public boolean isBorrowed() {
-        return borrowed;
-    }
 }
